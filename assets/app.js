a3b13711bf80ce55758b4f5b71f45b54d768b2f5
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-29 22:08:11 -0400

    Notes-App

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+const noteManager({
+    el:document.getElementById('notesWraper'),
+    notes: [
+        {
+            title: 'How was your day',
+            body: 'The day went well'
+        },
+        {
+            title:
+            body:
+        },
+        {
+            title:
+            body:
+        },
+        {
+            title: 
+            body:
+        },
+        {
+            title:
+            body:
+        }
+    ]
+})
